7737d15cd5bf8cc09b7228d635a9867c3e39e6e4
pgwstd <2712278005@qq.com>
2023-03-17 17:01:19 +0800

    Get

---
+++ Ajax学习/js/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/server', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');// 设置响应头，允许跨域
+    res.send('Hello Ajax');// 响应数据
+});
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
