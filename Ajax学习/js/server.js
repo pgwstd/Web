12208939efb235038b9027a6aad41dfb11f6f1e6
pgwstd <2712278005@qq.com>
2023-03-18 21:17:49 +0800

    Timeout

--- Ajax学习/js/server.js
+++ Ajax学习/js/server.js
@@ -11,10 +11,18 @@ app.get('/json-server', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');// 设置响应头，允许跨域
     res.setHeader('Access-Control-Allow-Headers', '*');// 设置响应头，允许跨域
     const data = {
-        name: 'panguangwei'
+        name: 'panguangwei-2'
     }
     let str = JSON.stringify(data);
     res.send(str);// 响应数据
+});
+
+app.get('/delay', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');// 设置响应头，允许跨域
+    res.setHeader('Access-Control-Allow-Headers', '*');// 设置响应头，允许跨域
+    setTimeout(()=>{
+        console.log("发送数据");
+    },3000);
 });
 
 app.post('/server', (req, res) => {
