188043675c919f0e5f7d8edd93efe1a51ceae677
pgwstd <2712278005@qq.com>
2023-03-20 15:47:55 +0800

    File

--- Node学习/js/File.js
+++ Node学习/js/File.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 
 // console.log(fs);
 
+/*
 //打开
 let fd = fs.openSync("test.txt", "w");
 
@@ -9,4 +10,23 @@ let fd = fs.openSync("test.txt", "w");
 fs.writeSync(fd, "今天天气真不错");
 
 //保存关闭
-fs.closeSync(fd);
+fs.closeSync(fd);*/
+
+
+fs.open("test2.txt", "w", function (err, fd) {
+    if (!err) {
+        fs.write(fd, "今天出去玩", function (err) {
+            if (!err) {
+                console.log("写入成功");
+            }
+            fs.close(fd, function (err) {
+                if (!err) {
+                    console.log("保存成功");
+                }
+            });
+
+        });
+    } else {
+        console.log(err);
+    }
+});
