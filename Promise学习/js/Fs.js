f3a74619b3dfac95ee2ee83d6f3a564a6a19dcaf
pgwstd <2712278005@qq.com>
2023-03-21 20:44:07 +0800

    Fs

---
+++ Promise学习/js/Fs.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+//回调函数模式
+/*
+fs.readFile('/Users/panguangwei/WebGithub/Promise学习/test.txt', (err, data) => {
+   if (err) throw err;
+   console.log(data.toString());
+});*/
+
+
+//使用Promise方式
+let p = new Promise((resolve, reject) => {
+    fs.readFile('/Users/panguangwei/WebGithub/Promise学习/test.txt', (err, data) => {
+        if (err) reject(err);
+        resolve(data);
+    });
+});
+
+p.then(value => {
+    console.log(value.toString());
+}, reason => {
+    console.log(reason);
+});
