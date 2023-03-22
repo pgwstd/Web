4b283e570149bf79186f3e8ab9e12acb7b7c2073
pgwstd <2712278005@qq.com>
2023-03-22 16:41:28 +0800

    Promise自定义封装

--- Promise学习/js/Promise.js
+++ Promise学习/js/Promise.js
@@ -20,7 +20,7 @@ function Promise(executor) {
         //判断状态让它的状态只改变一次
         if (_this.PromiseState !== 'pending') return;
         //1.修改对象的状态(promiseState)
-        _this.PromiseState = 'reject';
+        _this.PromiseState = 'rejected';
         //2.设置对象结果值(promiseResult)
         _this.PromiseResult = data;
     }
@@ -33,5 +33,11 @@ function Promise(executor) {
 }
 
 Promise.prototype.then = function (onResolved, onReject) {
-
+    //回调的执行
+    if (this.PromiseState === 'fulfilled'){
+        onResolved(this.PromiseResult);
+    }
+    if (this.PromiseState === 'rejected'){
+        onReject(this.PromiseResult);
+    }
 }
