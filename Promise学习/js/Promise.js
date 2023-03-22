3470fd3f24b0d4ff10a68c8f6fb2eb7c7016a5ed
pgwstd <2712278005@qq.com>
2023-03-22 16:26:10 +0800

    Promise封装

--- Promise学习/js/Promise.js
+++ Promise学习/js/Promise.js
@@ -1,13 +1,23 @@
 //重写Promise
 function Promise(executor){
+    //添加属性
+    this.PromiseState = 'pending';
+    this.PromiseResult = null;
+    const _this = this;
     //resolve函数
     function resolve(data){
-
+        //1.修改对象的状态(promiseState)
+        _this.PromiseState = 'fulfilled';
+        //2.设置对象结果值(promiseResult)
+        _this.PromiseResult = data;
     }
 
     //reject函数
     function reject(data){
-
+        //1.修改对象的状态(promiseState)
+        _this.PromiseState = 'reject';
+        //2.设置对象结果值(promiseResult)
+        _this.PromiseResult = data;
     }
     executor(resolve, reject);
 }
