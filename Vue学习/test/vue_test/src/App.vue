3c28057f3be1ad144ac9ffb8d93abbcf1a1ff894
潘光伟 <2712278005@qq.com>
2023-05-30 23:00:41 +0800

    单文件模式

---
+++ Vue学习/test/vue_test/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <img alt="Vue logo" src="./assets/logo.png">
+    <School/>
+    <Student/>
+  </div>
+</template>
+
+<script>
+import School from './components/MySchool.vue'
+import Student from './components/MyStudent.vue'
+
+export default {
+    name: 'App',
+    components:{
+        School,
+        Student
+    }
+}
+</script>
---
+++ Vue学习/test/vue_test/src/components/MyStudent.vue
@@ -0,0 +1,22 @@
+<template>
+<!--  组件的结构-->
+<div class="demo">
+    <h2>学生姓名:{{name}}</h2>
+    <h2>年龄:{{age}}</h2>
+</div>
+</template>
+
+
+<script>
+// 组件交互的逻辑
+export default {
+    name: 'MyStudent',
+    data(){
+        return {
+            name:'张三',
+            age:19
+        }
+    }
+}
+
+</script>
