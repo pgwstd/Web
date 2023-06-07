a915c12d36d9e8ec810dd2c3dc50a73e710fff6f
潘光伟 <2712278005@qq.com>
2023-06-07 22:34:25 +0800

    更新

---
+++ Vue学习/day02/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <h1 v-text="msg" ref="title"></h1>
+        <button @click="changeMsg">点击改变标题</button>
+        <MySchool/>
+    </div>
+</template>
+
+<script>
+import MySchool from "@/components/MySchool.vue";
+
+export default {
+    name: 'App',
+    components: {
+        MySchool
+    },
+    data() {
+        return {
+            msg: 'hello vue'
+        }
+    },
+    methods: {
+        changeMsg() {
+            this.msg = 'hello world'
+            console.log(this.$refs.title)
+            this.$refs.title.style.color = 'red'
+        }
+    }
+}
+</script>
+
---
+++ Vue学习/day02/src/components/MySchool.vue
@@ -0,0 +1,38 @@
+<template>
+<!--  组件的结构-->
+<div class="demo">
+    <h2>学校名称:{{name}}</h2>
+    <h2>学校地址:{{address}}</h2>
+    <button @click="showName">点击显示学校名</button>
+</div>
+</template>
+
+
+<script>
+// 组件交互的逻辑
+export default {
+    name: 'MySchool',
+    data(){
+        return {
+            name:'清华大学',
+            address:'北京市海淀区'
+        }
+    },
+    methods:{
+        showName(){
+            alert(this.name)
+        }
+    }
+}
+
+</script>
+
+<style>
+/*组件的样式*/
+.demo{
+    width: 300px;
+    height: 300px;
+    background-color: #a0ff00;
+}
+
+</style>
