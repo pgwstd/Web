c715e51906a3023766c2ec8c0cb2526b485814bd
潘光伟 <2712278005@qq.com>
2023-06-12 19:08:40 +0800

    scoped属性

---
+++ Vue学习/day05/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <MySchool />
+      <MyStudent />
+  </div>
+</template>
+
+<script>
+
+import MySchool from "@/components/MySchool.vue";
+import MyStudent from "@/components/MyStudent.vue";
+
+export default {
+  name: 'App',
+  components: {
+      MyStudent,
+      MySchool,
+  }
+}
+</script>
+
+
+
